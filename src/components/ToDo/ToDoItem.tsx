707369740153c895421c7fd507ec32e771f903ee
E-rar <erik.ramich@gmx.de>
2021-12-08 03:23:43 +0100

    push

---
+++ src/components/ToDo/ToDoItem.tsx
@@ -0,0 +1,27 @@
+import './ToDoItem.scss';
+
+type TodoTypes={
+    content:string
+    check:boolean
+}
+export const ToDo  = (props:TodoTypes) => {
+    return ( 
+        <div className='item'>
+            <div className='checkbox' style={{backgroundColor:props.check?'green':'red'}}>
+                
+            </div>
+            <div className="contentContainer">
+                <h3 className="content">
+                    {props.content}
+                </h3>
+            </div>
+            
+            <div className="drag">
+                ..<br/>
+                ..<br/>
+                ..<br/>
+            </div>
+        </div>
+     )
+}
+export default ToDo;
